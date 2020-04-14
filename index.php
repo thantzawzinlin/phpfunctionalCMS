4a640a5be8c73875b31327306d3a4dd6b7b12e61
unknown <thantzawzinlin@gmail.com>
2020-04-14 15:45:21 +0630

    displaying data from database for posts and categories

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php 
+    include ('includes/header.php');
+?>
+<body>
+
+    <!-- Navigation -->
+<?php
+    include('includes/db.php');
+    include('includes/navbar.php');
+?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+            <?php 
+            $query="SELECT * FROM posts";
+
+            $output_post_data=mysqli_query($connection,$query);
+
+            while($row=mysqli_fetch_assoc($output_post_data)){
+                $post_title=$row['post_title'];
+                $post_author=$row['post_author'];
+                $post_date=$row['post_date'];
+                $post_tags=$row['post_tags'];
+                $post_content=$row['post_content'];
+                $post_image=$row['post_image'];
+              ?>  
+
+                        <h1 class="page-header">
+                             Page Heading
+                            <small>Secondary Text</small>
+                        </h1>
+
+                        <!-- First Blog Post -->
+                        <h2>
+                            <a href="#"><?php echo $post_title ?></a>
+                        </h2>
+                        <p class="lead">
+                            by <a href="index.php"><?php echo $post_author?></a>
+                        </p>
+                        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date?></p>
+                        <hr>
+                        <img class="img-responsive" src="images/<?php echo $post_image?>" alt="<?php echo $post_title ?>">
+                        <hr>
+                        <p><?php echo $post_content?></p>
+                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+
+
+          <?php  } ?>
+
+               
+
+
+                <!-- Pager -->
+                <ul class="pager">
+                    <li class="previous">
+                        <a href="#">&larr; Older</a>
+                    </li>
+                    <li class="next">
+                        <a href="#">Newer &rarr;</a>
+                    </li>
+                </ul>
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+           <?php 
+           include('includes/sidebar.php');
+           ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+       <?php 
+
+       include('includes/footer.php');
+       ?>
